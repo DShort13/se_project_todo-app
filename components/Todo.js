69e8ff151c867b60b2fdd25f29841c12484cb026
DShort13 <dion.short@outlook.com>
2024-08-15 22:07:35 +0100

    create setEventListeners function in todo.js

--- components/Todo.js
+++ components/Todo.js
@@ -4,22 +4,37 @@ class Todo {
     this._templateElement = document.querySelector(selector);
   }
 
+  _setEventListeners() {
+    this.todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _generateCheckboxEl() {
+    this.todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this.todoLabel = this._todoElement.querySelector(".todo__label");
+    this.todoCheckboxEl.checked = this._data.completed;
+    this.todoCheckboxEl.id = `todo-${this._data.id}`;
+    this.todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
   getView() {
     this._todoElement = this._templateElement.content
       .querySelector(".todo")
       .cloneNode(true);
 
     const todoNameEl = this._todoElement.querySelector(".todo__name");
-    const todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
-    const todoLabel = this._todoElement.querySelector(".todo__label");
     const todoDate = this._todoElement.querySelector(".todo__date");
-    const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
 
     todoNameEl.textContent = this._data.name;
-    todoCheckboxEl.checked = this._data.completed;
 
-    todoCheckboxEl.id = `todo-${this._data.id}`;
-    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    this._generateCheckboxEl();
+    this._setEventListeners();
 
     return this._todoElement;
   }
