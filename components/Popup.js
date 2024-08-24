9398fc199b8623e466df09202aa932fd24ca490b
DShort13 <dion.short@outlook.com>
2024-08-24 10:19:25 +0100

    add Popup class method to close popup using escape key

--- components/Popup.js
+++ components/Popup.js
@@ -4,15 +4,21 @@ class Popup {
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
+  _handleEscapeClose = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  };
+
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keydown", this._handleEscapeClose);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
+    document.removeEventListener("keydown", this._handleEscapeClose);
   }
-
-  // _handleEscapeClose() {}
 
   setEventListeners() {
     this._popupCloseBtn.addEventListener("click", () => {
